bc46a31c938d78b340557ee03a0746baabcf8016
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2024-11-11 17:42:24 -0500

    Spline cleanup

--- src/util/spline.hpp
+++ src/util/spline.hpp
@@ -4,6 +4,7 @@
 #include "format.hpp"
 #include "vector.hpp"
 
+// Inspired by https://stackoverflow.com/a/23258882
 class Spline2D {
 public:
     Spline2D() = default;
@@ -19,6 +20,8 @@ protected:
     std::vector<Vector2D> m_knots;
 };
 
+// https://www.mvps.org/directx/articles/catmull/
+// https://qroph.github.io/2018/07/30/smooth-paths-using-catmull-rom-splines.html
 class CatmullRomSpline2D : public Spline2D {
 public:
     CatmullRomSpline2D() = default;
@@ -42,77 +45,83 @@ public:
     }
 };
 
-// https://stackoverflow.com/a/23258882
 class CubicSpline2D : public Spline2D {
 public:
     CubicSpline2D() = default;
     CubicSpline2D(const std::vector<Vector2D>& knots) : Spline2D(knots) {}
     ~CubicSpline2D() = default;
 
-    Vector2D operator()(const Index segment, const f64 t) {
-        assert(segment >= 0);
-        assert(segment < m_knots.size() - 1);
+    Vector2D operator()(const Index i, const f64 t) {
+        assert(i >= 0);
+        assert(i < m_knots.size() - 1);
         assert(t >= 0.0);
         assert(t <= 1.0);
 
-        const f64 dx = m_knots[segment + 1].x - m_knots[segment].x;
-        const f64 x = t * dx + m_knots[segment].x;
+        const f64 h = m_knots[i + 1].x - m_knots[i].x;
+        const f64 x = t * h + m_knots[i].x;
+        const f64 xi1_x = m_knots[i + 1].x - x;
+        const f64 x_xi = x - m_knots[i].x;
 
-        const f64 t3 = t * t * t - t;
-        const f64 t_inv = 1.0 - t;
-        const f64 t_inv3 = t_inv * t_inv * t_inv - t_inv;
-        const f64 mix = t * m_knots[segment + 1].y + t_inv * m_knots[segment].y;
-        const f64 y = mix + 1.0 / 6.0 * dx * dx *
-                                (t3 * m_x[segment + 1] - t_inv3 * m_x[segment]);
+        const f64 t1 = m_x[i] * xi1_x * xi1_x * xi1_x / (6.0 * h);
+        const f64 t2 = m_x[i + 1] * x_xi * x_xi * x_xi / (6.0 * h);
+
+        const f64 b = m_knots[i].y / h - m_x[i] * h / 6.0;
+        const f64 t3 = b * xi1_x;
+
+        const f64 c = m_knots[i + 1].y / h - m_x[i + 1] * h / 6.0;
+        const f64 t4 = c * x_xi;
+
+        const f64 y = t1 + t2 + t3 + t4;
 
         return {x, y};
     }
 
     void solve() override {
-        m_diag.resize(m_knots.size());
-        m_offDiag.resize(m_knots.size());
         m_x.resize(m_knots.size());
-        m_b.resize(m_knots.size());
+
+        std::vector<f64> diag(m_knots.size());
+        std::vector<f64> off_diag(m_knots.size());
+        std::vector<f64> rhs(m_knots.size());
 
         Index n = m_knots.size();
 
-        // Use the Thomas algorithm to solve the tridiagonal matrix
-        // https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
-        for (Index i = 1; i < n - 1; ++i)
-            m_diag[i] = 2.0 * (m_knots[i + 1].x - m_knots[i - 1].x);
-        m_diag[0] = 2.0 * (m_knots[1].x - m_knots[0].x);
-        m_diag[n - 1] = 2.0 * (m_knots[n - 1].x - m_knots[n - 2].x);
-
-        for (Index i = 0; i < n - 1; ++i)
-            m_offDiag[i] = m_knots[i + 1].x - m_knots[i].x;
-
+        // Create the system
+        off_diag[0] = m_knots[1].x - m_knots[0].x;
         for (Index i = 1; i < n - 1; ++i) {
-            // m_offDiag[i] corresponds to h_i
-            const f64 l = (m_knots[i + 1].y - m_knots[i].y) / m_offDiag[i];
-            const f64 r = (m_knots[i].y - m_knots[i - 1].y) / m_offDiag[i - 1];
-            m_b[i] = 6.0 * (l - r);
+            diag[i] = 2.0 * (m_knots[i + 1].x - m_knots[i - 1].x);
+
+            off_diag[i] = m_knots[i + 1].x - m_knots[i].x;
+
+            // off_diag[i] corresponds to h_i
+            const f64 l = (m_knots[i + 1].y - m_knots[i].y) / off_diag[i];
+            const f64 r = (m_knots[i].y - m_knots[i - 1].y) / off_diag[i - 1];
+            rhs[i] = 3.0 * (l - r);
         }
 
-        // Use natural spline conditions (v_0 = v_n = 0)
-        m_b[0] = 6.0 * (m_knots[1].y - m_knots[0].y) / m_offDiag[0];
+        // Boundary conditions
+        diag[0] = 2.0 * (m_knots[1].x - m_knots[0].x);
+        diag[n - 1] = 2.0 * (m_knots[n - 1].x - m_knots[n - 2].x);
 
-        // Replace m_diag and m_b in-place
+        // Use natural spline conditions (v_0 = v_n = 0)
+        rhs[0] = 3.0 * (m_knots[1].y - m_knots[0].y) / off_diag[0];
+        rhs[n - 1] =
+            -3.0 * (m_knots[n - 1].y - m_knots[n - 2].y) / off_diag[n - 2];
+
+        // Solve using in-place Thomas algorithm.
+        // https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
         for (Index i = 1; i < n; ++i) {
-            const f64 w = m_offDiag[i] / m_diag[i - 1];
-            m_diag[i] -= w * m_diag[i - 1];
-            m_b[i] -= w * m_b[i - 1];
+            const f64 w = off_diag[i - 1] / diag[i - 1];
+            diag[i] -= w * diag[i - 1];
+            rhs[i] -= w * rhs[i - 1];
         }
 
         // Back substitution
-        m_x[n - 1] = m_b[n - 1] / m_diag[n - 1];
-        for (i64 i = n - 1; i-- > 0;) {
-            m_x[i] = (m_b[i] - m_offDiag[i] * m_x[i + 1]) / m_diag[i];
+        m_x[n - 1] = rhs[n - 1] / diag[n - 1];
+        for (Index i = n - 1; i-- > 1;) {
+            m_x[i] = (rhs[i] - off_diag[i] * m_x[i + 1]) / diag[i];
         }
     }
 
 private:
-    std::vector<f64> m_diag;
-    std::vector<f64> m_offDiag;
     std::vector<f64> m_x;
-    std::vector<f64> m_b;
 };
