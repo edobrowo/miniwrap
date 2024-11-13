83b2a291f86fe626e5b83e745aa2ec85d111757d
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2024-11-12 23:54:21 -0500

    Clean up math

--- src/util/math.hpp
+++ src/util/math.hpp
@@ -6,16 +6,20 @@
 
 namespace math {
 
-constexpr f64 Pi = 3.14159265358979323846;
-constexpr f64 PiH = 1.57079632679489661923;
-constexpr f64 PiQ = 0.78539816339744830961;
-constexpr f64 InvPi = 0.31830988618379067154;
-constexpr f64 Tau = 6.28318530717958647692;
+// clang-format off
+
+constexpr f64 Pi     = 3.14159265358979323846;
+constexpr f64 PiH    = 1.57079632679489661923;
+constexpr f64 PiQ    = 0.78539816339744830961;
+constexpr f64 InvPi  = 0.31830988618379067154;
+constexpr f64 Tau    = 6.28318530717958647692;
 constexpr f64 InvTau = 0.15915494309189533577;
+
+// clang-format on
 
 template <Numeric N>
 constexpr inline N step(N value, N threshold) {
-    return value < threshold ? 0 : 1;
+    return value < threshold ? 0.0 : 1.0;
 }
 
 template <Numeric N>
@@ -29,11 +33,7 @@ constexpr inline N clamp(N value, N left, N right) {
 
 template <std::floating_point F>
 constexpr inline F unit_clamp(F value) {
-    if (value < 0.0)
-        return 0.0;
-    else if (value > 1.0)
-        return 1.0;
-    return value;
+    return clamp(value, 0.0, 1.0);
 }
 
 template <std::floating_point F>
@@ -50,7 +50,7 @@ constexpr inline N scale(N value, N min, N max) {
 template <Numeric N>
 constexpr inline N map(N value, N min1, N max1, N min2, N max2) {
     assert(max1 - min1 != 0);
-    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+    return min2 + (value - min1) / (max1 - min1) * (max2 - min2);
 }
 
 template <Numeric N>
@@ -60,7 +60,7 @@ constexpr inline N hermite(N value) {
 
 template <Numeric N>
 constexpr inline N smoothstep(N left, N right, N t) {
-    return hermite(clamp(scale(t, left, right)));
+    return hermite(unit_clamp(scale(t, left, right)));
 }
 
 template <std::integral I>
